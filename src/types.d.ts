bdab23de2a9d5f51fae6df97e18ae68816a29d66
idrodrigo <idrodrigo@icloud.com>
2023-07-20 22:33:34 -0600

    rm un type

--- src/types.d.ts
+++ src/types.d.ts
@@ -10,7 +10,6 @@ export type TodoId = Pick<Todo, 'id'>
 export type TodoTitle = Pick<Todo, 'title'>
 export type TodoCompleted = Pick<Todo, 'completed'>
 
-export type ListOfTodos = Todo[]
 export type FilterOption = typeof TODO_FILTERS[keyof typeof TODO_FILTERS]
 
 export type TodoList = Todo[]
